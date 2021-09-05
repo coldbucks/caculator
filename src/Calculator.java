527ad50409323220aed921829ea96e93880f1c48
'coldbucks' <'met.hsw@gmail.com'>
2021-09-05 23:22:41 +0900

    initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,10 @@
+
+public class Calculator {
+	public static void main(String[] args) {
+		System.out.println("µ¡¼À :" + (3 + 4));
+		System.out.println("»¬¼À :" + (4 - 3));
+		System.out.println("°ö¼À :" + (3 * 4));
+		System.out.println("³ª´°¼À :" + (4 / 2));
+	
+	}
+}
